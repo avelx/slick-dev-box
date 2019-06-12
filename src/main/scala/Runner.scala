0c8a146c3d3f56c6549c507948cf22ff92468a8e
avelreserch <pavel.oliynyk@gmail.com>
2019-06-12 19:08:26 +0100

    Add streaming example@

--- src/main/scala/Runner.scala
+++ src/main/scala/Runner.scala
@@ -2,9 +2,10 @@ package com.avelimited.slick.demo
 
 import slick.jdbc.H2Profile.api._
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
+import scala.util.{Failure, Success, Try}
 
 object Runner {
 
@@ -21,17 +22,56 @@ object Runner {
 
       println(res)
 
-      println("Coffees:")
-      db.run(coffees.result).map(_.foreach {
-        case (name, supID, price, sales, total) =>
-          println("  " + name + "\t" + supID + "\t" + price + "\t" + sales + "\t" + total)
-      })
+      // Querying 1.
+      //      println("Coffees:")
+      //      db.run(coffees.result).map(_.foreach {
+      //        case (name, supID, price, sales, total) =>
+      //          println("  " + name + "\t" + supID + "\t" + price + "\t" + sales + "\t" + total)
+      //      })
+
+      val ins1: DBIO[Int] = coffees += ("ColombianData", 101, 17.99, 0, 0)
+      val ins2: DBIO[Int] = coffees += ("French_RoastData", 101, 87.99, 0, 0)
+
+      val a1: DBIO[Unit] = DBIO.seq(ins1, ins2)
+
+      val a2: DBIO[Int] = ins1 andThen ins2
+
+      val a3: DBIO[(Int, Int)] = ins1 zip ins2
+
+      val a4: DBIO[Vector[Int]] = DBIO.sequence(Vector(ins1, ins2))
+
+      val f: Future[_] = db.run(a3)
+
+      f.onComplete {
+        case Success(s) => println(s"Result: $s")
+        case Failure(t) => t.printStackTrace()
+      }
+
+      Thread.sleep(1000)
+
+      // Why not let the database do the string conversion and concatenation?
+      val q1 = for{
+        c <- coffees
+        //if (c.price > 9.0)
+      } yield LiteralColumn("  ") ++ c.name ++ "\t" ++ c.supID.asColumnOf[String] ++
+          "\t" ++ c.price.asColumnOf[String] ++ "\t" ++ c.sales.asColumnOf[String] ++
+          "\t" ++ c.total.asColumnOf[String]
+      // The first string constant needs to be lifted manually to a LiteralColumn
+      // so that the proper ++ operator is found
+
+      // Equivalent SQL code:
+      // select '  ' || COF_NAME || '\t' || SUP_ID || '\t' || PRICE || '\t' SALES || '\t' TOTAL from COFFEES
+
+
+
+      val source = db
+        .stream(q1.result)
+        .foreach(println)
 
 
       Thread.sleep(2000)
       //println(setupFuture)
     } finally db.close
-
 
 
   }
